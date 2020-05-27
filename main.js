a2f7033c5a3da75f2058aeef8fb9e22a8d081606
YongCoding <aaa9015@naver.com>
2020-05-27 22:44:14 +0900

    Last touch (First)

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ navbarMenu.addEventListener('click', (event) => {
 
 // Navbar toggle button for small screen
 const navbarToggleBtn=document.querySelector('.navbar__toggle-btn');
+
 navbarToggleBtn.addEventListener('click', () => {
 navbarMenu.classList.toggle('open');
 });
